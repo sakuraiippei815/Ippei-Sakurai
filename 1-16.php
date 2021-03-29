7495597866c9ffe08d3abb1330fcb7688625e66a
sakuraiippei815 <81559443+sakuraiippei815@users.noreply.github.com>
2021-03-29 18:57:51 +0900

    Add files via upload

---
+++ 1-16.php
@@ -0,0 +1,5 @@
+<?phpecho var_dump(true);echo"<br>";
+     echo var_dump(false);echo"<br>";
+      $number=4 ;echo var_dump($number==4);
+      echo"<br>";
+                 echo var_dump($number!=4);?>
